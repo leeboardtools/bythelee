7b4065b6192093828c51cf2c71c093ab44b74523
Albert Santos <leeboardtools@gmail.com>
2017-11-23 07:54:50 -0500

    Updated the copyright headers to say Albert Santos instead of just albert.

--- public_html/js/leeboard/sailsim-three/Sky3D.js
+++ public_html/js/leeboard/sailsim-three/Sky3D.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2017 albert.
+ * Copyright 2017 Albert Santos.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,14 @@ LBSailSim.Sky3D.prototype = {
             'pz.png', 'nz.png'
         ]);
         
+        // Sky-box arrangement:
+        //          +----+
+        //          | py |
+        //     +----+----+----+----+
+        //     | pz | px | nz | nx |
+        //     +----+----+----+----+
+        //          | ny |
+        //          +----+
         
         var cubeShader = THREE.ShaderLib[ 'cube' ];
         cubeShader.uniforms[ 'tCube' ].value = this.skyTextureCube;
