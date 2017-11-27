6a30f9a85ebf82e38114d0c990a1150456306e64
Albert Santos <leeboardtools@gmail.com>
2017-11-27 11:27:40 -0500

    Converted to a Node.js Express server.

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public_html
+//app.use(favicon(path.join(__dirname, 'public_html', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+//
+//-------------------------------------------------------------------
+// My stuff...
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('disconnected');
+    });
+    
+    socket.on('debug-timing', function(data) {
+        console.log('');
+        var now = new Date();
+        console.log('debug-timing:' + now.toTimeString());
+        if (data.fps) {
+            console.log('FPS: ' + data.fps.toFixed(1));
+        }
+        var sum = 0;
+        Object.keys(data).forEach(function(key) {
+            var record = data[key];
+            if (key !== 'fps') {
+                        console.log(key + ': ' + record.maxMs.toFixed(2) + ' ' + record.frozenMs.toFixed(2) + ' ' + record.averageMs.toFixed(2) + ' ' + record.count);
+                sum += record.averageMs;
+            }
+        });
+        console.log('total avg. time: ' + sum.toFixed(2));
+    });
+});
+
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var port = 3000;
+var listeningMsg = 'Listening on ' + port;
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            listeningMsg = "Listening on " + address.address + ':' + port;
+            break;
+        }
+    }
+}
+
+http.listen(port, function() {
+    console.log(listeningMsg);
+});
+
+module.exports = app;
